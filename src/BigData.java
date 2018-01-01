7cb5ad2b68a3e63cab12f4eeda04f2f7536c664c
mosharrofrubel <rubel007cse@gmail.com>
2018-01-01 12:27:32 +0600

    Big data codes added

---
+++ src/BigData.java
@@ -0,0 +1,16 @@
+/**
+ * Created by mosharrofrubel on 1/1/18.
+ */
+public class BigData {
+
+    BigData(String s) {
+        System.out.print(s);
+    }
+
+
+    public static void main(String[] args){
+
+        BigData obj = new BigData("Name of the data");
+
+    }
+}
